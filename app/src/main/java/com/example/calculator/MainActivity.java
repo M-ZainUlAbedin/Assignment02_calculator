169c056918dc097c3b2d46583c208d2b2acb8594
SyedShahidArshad <bitf18a528@pucit.edu.pk>
2021-10-24 14:05:05 +0500

    Finishing_designing

--- app/src/main/java/com/example/calculator/MainActivity.java
+++ app/src/main/java/com/example/calculator/MainActivity.java
@@ -3,9 +3,11 @@ package com.example.calculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class MainActivity extends AppCompatActivity {
-
+    Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
+    Button btnminus, btnplus,btnmulti,btndvd,btnclr,btndot,btnequal;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
